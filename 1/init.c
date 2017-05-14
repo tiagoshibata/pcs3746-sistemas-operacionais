47d3b3776ab47bfe0ac7278c8b5e6db7e264f34a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-05-14 00:59:50 -0300

    Initial support for custom syscalls insert/remove

--- 1/init.c
+++ 1/init.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "stack.h"
 
 int main(int argc, char **argv) {
 	printf("Hello\n");
+	printf("%d\n", insert(15));
 	for (;;) ;
 }
---
+++ 1/stack.h
@@ -0,0 +1,17 @@
+#include<linux/unistd.h>
+
+#define __NR_insert 398
+#define __NR_remove 399
+
+long insert(int value)
+{
+	return syscall(__NR_insert, value);
+}
+
+long remove()
+{
+	return syscall(__NR_remove, value);
+}
+
+// _syscall1(long, insert, int, value)  // FIXME Macros are cleaner
+// _syscall0(long, remove)

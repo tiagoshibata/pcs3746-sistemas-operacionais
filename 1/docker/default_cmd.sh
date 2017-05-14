0c7a85d86abab3b538bf706da6d51e7c7f573126
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-05-13 21:16:50 -0300

    Add sample initramfs to docker

--- 1/docker/default_cmd.sh
+++ 1/docker/default_cmd.sh
@@ -6,8 +6,9 @@ fail() {
     exit 1
 }
 
-if ! cd /home/student/src/linux ; then
-    fail "Mount a linux tree under /home/student/src/linux"
+SRC=/home/student/src
+if ! cd $SRC/linux ; then
+    fail "Mount a linux tree volume under $SRC/linux"
 fi
 
 if [ ! -f .config ] ; then
@@ -15,5 +16,12 @@ if [ ! -f .config ] ; then
     make ARCH=arm versatile_defconfig
     make ARCH=arm silentoldconfig
 fi
-
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
+
+if ! cd $SRC/initramfs ; then
+    fail "Mount a volume with initramfs source files under $SRC/initramfs"
+fi
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+
+BOOT=$SRC/linux/arch/arm/boot
+qemu-system-arm -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"

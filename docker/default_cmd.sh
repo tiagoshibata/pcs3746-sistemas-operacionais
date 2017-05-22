8793050a0e6545a1d9208b6d12daddc380ac432f
Felipe Goncalves Marques <felipe.goncalves@taqtil.com>
2017-05-22 08:49:54 -0300

    Added System Call and made example to use it

--- docker/default_cmd.sh
+++ docker/default_cmd.sh
@@ -7,18 +7,18 @@ fail() {
 }
 
 SRC=/home/student/src
-if ! cd $SRC/linux ; then
-    fail "Mount a linux tree volume under $SRC/linux"
-fi
+# if ! cd $SRC/linux ; then
+#     fail "Mount a linux tree volume under $SRC/linux"
+# fi
 
-if [ ! -f .config ] ; then
-    echo ".config not found, using versatile template"
-    make ARCH=arm versatile_defconfig
-    # Enable debug symbols
-    sed -i -e 's/.*CONFIG_DEBUG_INFO.*/CONFIG_DEBUG_INFO=y/' .config
-    make ARCH=arm olddefconfig
-fi
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
+# if [ ! -f .config ] ; then
+#     echo ".config not found, using versatile template"
+#     make ARCH=arm versatile_defconfig
+#     # Enable debug symbols
+#     sed -i -e 's/.*CONFIG_DEBUG_INFO.*/CONFIG_DEBUG_INFO=y/' .config
+#     make ARCH=arm olddefconfig
+# fi
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
 
 if ! cd $SRC/initramfs ; then
     fail "Mount a volume with initramfs source files under $SRC/initramfs"

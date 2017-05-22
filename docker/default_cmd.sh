9a4ca2b2878eab0d9c6fcac96db2afbbf4f0143a
Felipe Goncalves Marques <felipe.goncalves@taqtil.com>
2017-05-22 15:54:47 -0300

    Added comments

--- docker/default_cmd.sh
+++ docker/default_cmd.sh
@@ -7,6 +7,7 @@ fail() {
 }
 
 SRC=/home/student/src
+# Linux compilation
 # if ! cd $SRC/linux ; then
 #     fail "Mount a linux tree volume under $SRC/linux"
 # fi
@@ -20,10 +21,12 @@ SRC=/home/student/src
 # fi
 # make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
 
+# Program compilation
 if ! cd $SRC/initramfs ; then
     fail "Mount a volume with initramfs source files under $SRC/initramfs"
 fi
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 
+# QEMU Running
 BOOT=$SRC/linux/arch/arm/boot
-qemu-system-arm -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"
+qemu-system-arm -s -S -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"

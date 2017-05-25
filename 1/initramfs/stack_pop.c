6e4f04d76a64421b6a7ac397e5a142fd5f74bb27
bertero <guilherme.bertero@gmail.com>
2017-05-25 20:00:17 -0300

    added print for stack statistics @stack_pop.c

--- 1/initramfs/stack_pop.c
+++ 1/initramfs/stack_pop.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include "stack.h"
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main()
 {
-	while(1) {
-		printf("%d\n", stack_pop());
-		sleep(rand() % 5 + 1);
-	}
-	return 0;
+    while(1) {
+        printf("stack_pop: %d\n", stack_pop());
+        sleep(rand() % 5 + 1);
+        int fd = open("/sys/kernel/sys_stack/count", O_RDONLY);
+        printf("stack_pop_count: %d\n", fd);
+    }
+    return 0;
 }

2a9bf5496bee422560fac4b204f6204a8f5de9ec
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-05-25 22:04:06 -0300

    Fix stack item count printing

--- 1/initramfs/stack_pop.c
+++ 1/initramfs/stack_pop.c
@@ -1,15 +1,28 @@
-#include <stdlib.h>
 #include "stack.h"
-#include <sys/stat.h>
+
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
-    while(1) {
-        printf("stack_pop: %d\n", stack_pop());
-        sleep(rand() % 5 + 1);
-        int fd = open("/sys/kernel/sys_stack/count", O_RDONLY);
-        printf("stack_pop_count: %d\n", fd);
-    }
-    return 0;
+	int fd = open("/sys/kernel/sys_stack/count", O_RDONLY);
+	char stack_count[5];
+
+	while(1) {
+		lseek(fd, 0, SEEK_SET);
+		int size = read(fd, stack_count, 4);
+		if (size < 0) {
+			perror("read");
+		} else {
+			stack_count[size] = 0;
+			printf("Stack item count: %s", stack_count);
+		}
+		printf("stack_pop: %ld\n", stack_pop());
+		sleep(rand() % 5 + 1);
+	}
+	return 0;
 }

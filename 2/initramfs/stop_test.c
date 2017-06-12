6363ab8bf99abc8cbf666100988fec75d80f4c48
Mateus Nakajo <mateusnakajo@gmail.com>
2017-06-11 23:41:01 -0300

    Add process that counts

--- 2/initramfs/stop_test.c
+++ 2/initramfs/stop_test.c
@@ -6,20 +6,34 @@
 
 int main()
 {
-	pid_t pid = fork();
-	if (pid == -1) {
+	pid_t pid_A = fork();
+	if (pid_A == -1) {
 		perror("fork");
 		return -1;
-	} else if (pid) {
-		for (;;) {
-			char c = getchar();
-			if (c == 'b') {
-				if (stop_process(pid))
-					return -1;
-			} else if (c == 'd') {
-				continue_process();
+	} else if (pid_A) {		
+		pid_t pid_int = fork();
+		if (pid_int == -1) {
+			perror("fork");
+			return -1;
+		} else if (pid_int) {
+			for (;;) {
+				char c = getchar();
+				if (c == 'b') {
+					if (stop_process(pid_A))
+						return -1;
+				} else if (c == 'a') {
+					stop_process(pid_int);
+				} else if (c == 'd') {
+					continue_process();
+				}
 			}
-		}
+		} else {
+			for (int i = 0;;i++) {
+				const struct timespec ts = {.tv_sec = 0, .tv_nsec = 5e8 };
+				nanosleep(&ts, NULL);
+				printf("%d\n", i);
+			}
+		}		
 	} else {
 		for (;;) {
 			const struct timespec ts = {.tv_sec = 0, .tv_nsec = 5e8 };

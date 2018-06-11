a417b0e71df7f91d35f7414a8b6fede277c13718
joaoCasella <jpedrocaselladepaula@hotmail.com>
2018-06-10 23:19:41 -0300

    Provisory delete

--- 1/initramfs/init.c
+++
@@ -1,9 +0,0 @@
-#include "hello_world.h"
-
-int main() {
-	while(1) {
-		hello_world();
-	}
-
-	return 0;
-}
--- 1/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -ti --rm -v "$PWD/linux":/home/student/src/linux -v "$PWD/initramfs":/home/student/src/initramfs tiagoshibata/pcs3746

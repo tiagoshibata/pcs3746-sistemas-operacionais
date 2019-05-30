11979144254938d8aa9d247858821f10a95f9600
ReMalbatan <renanmalbatan@hotmail.com>
2019-05-30 02:29:12 -0300

    Correcao para mostrar Race condition

--- 1/initramfs/read_write_number_2.c
+++ 1/initramfs/read_write_number_2.c
@@ -9,12 +9,14 @@ int main()
 	printf("Ola processo 2\n\n");
         time_t ltime; /* calendar time */
 	while(1) {
-		x++; 
+ 
+		x = read_number();
+		//ltime=time(NULL); /* get current cal time */
+		//	printf("%s || Processo 2: %d\n",asctime( localtime(&ltime)), x);
+		printf("%d\n", x);
+		sleep(1);
+		x++;
 		write_number(x);
-   		x = read_number();
-		ltime=time(NULL); /* get current cal time */
-		printf("\n%s || Processo 2: %d\n",asctime( localtime(&ltime)), x);
-   		sleep(1);
 	}
         return 0;
 }
